8be67d0f041dd343350a197daa6c3646e00be0af
Roberto Peregrina <111211515+RobertoPeregrinaJr96@users.noreply.github.com>
2022-11-22 19:11:23 -0800

    commit

---
+++ practice/basicInfo.js
@@ -0,0 +1,67 @@
+// copy and paste this in the terminal
+//(          node practice/               )
+
+// console.log() is a great debuging tool
+console.log("Hello World") // prints Hello World to the terminal
+
+// <--- these are single line code comments; use these for comments for your code
+
+/*
+
+ these are multi line comments
+
+*/
+
+// number type
+console.log("Number Type")
+console.log(1) // prints 1
+console.log(2) // prints 2
+console.log(3) // prints 3
+console.log(4) // prints 4
+console.log(5) // prints 5
+
+console.log("-----------------------------------")
+//Operators
+console.log("Operators")
+console.log(1 + 1) // prints 2      + adds the number
+console.log(2 - 1) // prints 1      - subtract's the number
+console.log(3 * 2) // prints 6      * multiply's the number
+console.log(4 / 2) // prints 2      / divides the number
+console.log(5 % 10) // prints 5     % modulo returns the remainder
+console.log("-----------------------------------")
+console.log("Boolean Operators")
+console.log(!true) // false        ! bang returns the opposite value
+console.log(true && false) // flase     && and has to have both be true to be true
+console.log(true || false) // true      || only needs one to be true to return true
+console.log("-----------------------------------")
+console.log("Comparison Operators")
+console.log(2 === 2) // true        === compares both elements strictly
+console.log(2 == "2") // true       == compares both elements loosly
+console.log(2 !== 1) // true        !== compares to see if there not the same
+console.log(2 > 1) // true      > checks if 2 is greater than 1
+console.log(2 < 1) // false     < checks if 2 is lesser than 1
+console.log(12 >= 12) // true    >= checks if 12 is greater than or equal to 12
+console.log(23 <= 43) // true    <= checks if 23 is lesser than or qual to 43
+
+console.log("-----------------------------------")
+
+console.log("Variables")
+// let number ;      <----- declares the variable // will return undefined as nothing was assigned to it
+// let number = 20  <----- the variable number is being initializing
+
+console.log("-----------------------------------")
+
+console.log("truthy and falsy")
+console.log("Truthy Values")
+console.log(2)      // has value
+console.log("true")     // has value
+console.log(true)       // is true
+console.log("Falsy Value")
+console.log(false)      // is false
+console.log(0)      // has no value
+console.log(-0)     // anything less than 0 is falsy
+console.log(0n)     // idk it just is
+console.log("")     // is a string but is empty
+console.log(null)       // absence of value
+console.log(undefined)      // isnt a value
+console.log(NaN)        // Not a number
